4d3124407de0696c03d137890a0c380c88aed442
Thea Olivia <theaolivia.mail@gmail.com>
2015-05-01 22:35:50 +0700

    Initial commit
    Pembagian tugas udah ada di sana ya

---
+++ poi-fs.c
@@ -0,0 +1,48 @@
+/* POI-Mounting */
+
+/* 
+	By Indische Partij
+	13511001 - Thea Olivia
+	13511051 - Andreas Dwi Nugroho
+	13511065 - Erwin
+*/
+
+	#include <stdio.h>
+	#include <stdlib.h>
+
+	/* *** FILE WAJIB *** */
+	// getattr - Thea
+	void GetAttr(){
+
+	}		
+	// readdir - Thea
+
+	// mkdir - Thea
+	
+	// mknod - Andre
+
+	// read - Erwin
+
+	// rmdir - Andre
+
+	// unlink - Erwin
+
+	// rename - Andre
+	
+	// write - Erwin
+	
+	// truncate - Relawan
+
+	
+	/* *** FILE BONUS *** */
+	
+	// chmod - Thea
+	
+	// link - Andre
+	
+	// open - Erwin
+
+/* MAIN FILE */
+int main(){
+	return 0;
+}
